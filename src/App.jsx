36d2cd3f77acd1dc58997726113b5813a65a8ca1
Defreitasnicolas <defreitasnicolas@gmail.com>
2023-10-11 12:06:46 +0200

    pokedex 4 passer des props

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
 import PokemonCard from "./components/PokemonCard";
 function Card() {
   return (
     <div>
-      <PokemonCard />
+      <PokemonCard pokemon={pokemonList[1]} />
     </div>
   )
 }
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,16 +1,8 @@
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
 
-function PokemonCard() {
-    const pokemon = pokemonList[0];
+
+function PokemonCard({pokemon}) {
+  
+   
     const imageok = pokemon.imgSrc ?(
       <img src={pokemon.imgSrc} alt={pokemon.name}/>
     ):(<p>???</p>);
